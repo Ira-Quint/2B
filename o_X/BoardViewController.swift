f1800d9755bfb6f930fc57cca13126e69e05aa33
Ira Quint <iraquint@Iras-MacBook-Pro.local>
2016-06-30 16:15:12 +0200

    added alert controllers

--- o_X/BoardViewController.swift
+++ o_X/BoardViewController.swift
@@ -11,60 +11,50 @@ class BoardViewController: UIViewController {
 
     @IBOutlet weak var boardView: UIView!
     
+    @IBOutlet weak var newGameOutlet: UIButton!
+    
     override func viewDidAppear(animated: Bool) {
         restartGame()
+        newGameOutlet.hidden = true
     }
     
     @IBAction func buttonPush(sender: UIButton) {
-        let numTag: Int = sender.tag
-        sender.setTitle(OXGameController.sharedInstance.playMove(numTag).rawValue, forState: .Normal)
-        sender.enabled = false
         
+        let cellVal = OXGameController.sharedInstance.playMove(sender.tag).rawValue
+        sender.setTitle(cellVal, forState: .Normal)
+        sender.userInteractionEnabled = false
         
-        var stateOfGame: OXGameState = OXGameController.sharedInstance.getCurrentGame().state()
+        let stateOfGame = OXGameController.sharedInstance.getCurrentGame().state()
         
-        if OXGameController.sharedInstance.getCurrentGame().gameWon() {
-            if (OXGameController.sharedInstance.getCurrentGame().turnCount() % 2 == 1) {
-                print ("X has won")
-            }
-            else {
-                print("O has won")
+        if stateOfGame == .InProgress {
+            return
+        }
+        
+        for view in boardView.subviews {
+            if let button = view as? UIButton {
+                button.userInteractionEnabled = false
             }
         }
         
-
-
-
-
-
-
-            /*let x: CellType = OXGameController.sharedInstance.getCurrentGame().playMove(sender.tag-1)
-            sender.setTitle(x.rawValue, forState: .Normal)
-            if (OXGameController.sharedInstance.getCurrentGame().state() == OXGameState.Won) {
-                print("\(x) Won")
-            }
-            if (OXGameController.sharedInstance.getCurrentGame().state() == OXGameState.Tie) {
-                print("The game is tied")
-            }
- */
-            
-    //}
-    
-        //If someone won the game, log a victorious message with the winner's name.
-        //If the game is tied, log a message saying the game was tied.
-        //After the game is ended restart the game (clear the board).
+        let message = (stateOfGame == .Won) ? "\(cellVal) has won!" : "It's a tie!"
         
-
-
+        let alert = UIAlertController (title: "Game Over", message: message, preferredStyle: .Alert)
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .Default, handler: { (action) in
+            
+            self.newGameOutlet.hidden = false
+            
+        })
+        
+        alert.addAction(dismissAction)
+        presentViewController(alert, animated: true, completion: nil)
     }
-    
 
     func restartGame() {
         OXGameController.sharedInstance.getCurrentGame().reset()
         for view in boardView.subviews {
             if let button = view as? UIButton {
                 button.setTitle("", forState: .Normal)
-                button.enabled = true
+                button.userInteractionEnabled = true
             }
         }
     }
@@ -73,14 +63,13 @@ class BoardViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
     @IBAction func newGameButtonPressed(sender: UIButton) {
         restartGame()
+        self.newGameOutlet.hidden = true
     }
-    
-    // Create additional IBActions here.
-    
     
     @IBAction func logOutButton(sender: AnyObject) {
         print("Log Out")
